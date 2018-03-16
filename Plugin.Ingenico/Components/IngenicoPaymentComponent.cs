44834705ab37e2cc90c59a9ba1fcafccd17bafc2
Jonne Kats <jonne.kats@avivasolutions.nl>
2018-03-16 11:42:47 +0100

    Add payment

---
+++ Plugin.Ingenico/Components/IngenicoPaymentComponent.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SampleComponent.cs" company="Sitecore Corporation">
+//   Copyright (c) Sitecore Corporation 1999-2017
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sitecore.Commerce.Plugin.Sample
+{
+    using Sitecore.Commerce.Core;
+    using Sitecore.Commerce.Plugin.Payments;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// The SampleComponent.
+    /// </summary>
+    public class IngenicoPaymentComponent : PaymentComponent
+    {
+        /// <summary>
+        /// Gets or sets the name of the brand used for the transaction.
+        /// </summary>
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the transaction.
+        /// </summary>
+        public string TransactionStatus { get; set; }
+
+        /// <summary>
+        /// The billing address party
+        /// </summary>
+        public Party BillingParty { get; set; }
+    }
+}
---
+++ Plugin.Ingenico/Controllers/CommandsController.cs
@@ -0,0 +1,64 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CommandsController.cs" company="Sitecore Corporation">
+//   Copyright (c) Sitecore Corporation 1999-2017
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sitecore.Commerce.Plugin.Sample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Web.Http.OData;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+    using Sitecore.Commerce.Core;
+    using Sitecore.Commerce.Plugin.Carts;
+    using Sitecore.Commerce.Plugin.Payments;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// Defines a controller
+    /// </summary>
+    /// <seealso cref="T:Sitecore.Commerce.Core.CommerceController" />
+    public class CommandsController : CommerceController
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Sitecore.Commerce.Plugin.Sample.CommandsController" /> class.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <param name="globalEnvironment">The global environment.</param>
+        public CommandsController(IServiceProvider serviceProvider, CommerceEnvironment globalEnvironment)
+            : base(serviceProvider, globalEnvironment)
+        {
+        }
+
+        [HttpPut]
+        [Route("AddIngenicoPayment()")]
+        public async Task<IActionResult> AddIngenicoPayment([FromBody] ODataActionParameters value)
+        {
+            CommandsController commandsController = this;
+            if (!commandsController.ModelState.IsValid || value == null)
+            {
+                return (IActionResult)new BadRequestObjectResult(commandsController.ModelState);
+            }
+
+            if (!value.ContainsKey("cartId") || value["cartId"] == null || !value.ContainsKey("payment") || value["payment"] == null)
+            {
+                return (IActionResult)new BadRequestObjectResult(value);
+            }
+
+            string cartId = value["cartId"].ToString();
+            IngenicoPaymentComponent paymentComponent = JsonConvert.DeserializeObject<IngenicoPaymentComponent>(value["payment"].ToString());
+            AddPaymentsCommand command = commandsController.Command<AddPaymentsCommand>();
+            Cart cart = await command.Process(commandsController.CurrentContext, cartId, new List<PaymentComponent>()
+            {
+                paymentComponent
+            });
+            return (IActionResult)new ObjectResult((object)command);
+        }
+    }
+}
+
